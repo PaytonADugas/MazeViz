a19c3836d8a80695ab406a21a4b666bc39500b5d
PaytonADugas <46801901+PaytonADugas@users.noreply.github.com>
2021-02-01 11:06:43 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PaytonSite</title>
+  </head>
+  <body>
+    <h1>Here is a lil title in h1 </h1>
+  </body>
+</html>
