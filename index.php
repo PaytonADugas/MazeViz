c89e5f457a961dab1a342bc2f9e5b703597e1569
PaytonADugas <46801901+PaytonADugas@users.noreply.github.com>
2021-02-11 00:17:03 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <title>PaytonSite</title>
   </head>
   <body>
-    <a href="css_demo.html>Link to CSS stuff</a>
-  <h1 class="display-4">Here is a h1 tag</h1>
-  <p class="lead">Here is a p tag</p>
+    <a href="css_demo.html">This is the demo file</a>
+    <h1 class="display-4">Here is a h1 tag</h1>
+    <p class="lead">Here is a p tag</p>
 </div>
   </body>
 </html>
