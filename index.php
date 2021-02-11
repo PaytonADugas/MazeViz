7f0003cf7565be5b1cf5fbb0a0b39de183ad7313
PaytonADugas <46801901+PaytonADugas@users.noreply.github.com>
2021-02-11 00:15:04 -0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
   </head>
   <body>
     <a href="css_demo.html>Link to CSS stuff</a>
-    <div class="jumbotron">
   <h1 class="display-4">Here is a h1 tag</h1>
   <p class="lead">Here is a p tag</p>
 </div>
