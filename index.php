6a5c6c1563b1c35724a7c3d07cca452564d0bed1
PaytonADugas <46801901+PaytonADugas@users.noreply.github.com>
2021-02-13 12:20:28 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,17 @@
   <head>
     <meta charset="utf-8">
     <title>PaytonSite</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Reggae+One&display=swap" rel="stylesheet">
   </head>
   <body>
     <a href="css_demo.html">This is the demo file</a>
     <h1 class="display-4">Here is a h1 tag</h1>
     <p class="lead">Here is a p tag</p>
+    
+    <p class="font_1">This is my fist font<p>
+    <P class="font_2">This is my second font<p>
+    <p class="font_3">This is my third font<p>
 </div>
   </body>
 </html>
